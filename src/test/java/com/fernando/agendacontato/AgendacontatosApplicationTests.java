1cadddc3bdc0515bea1b8daf34a81145181d5cf1
FernandoCostaB <fbc@icomp.ufam.edu.br>
2020-10-04 19:35:57 -0400

    Criação do Projeto de agenda de contatos

---
+++ src/test/java/com/fernando/agendacontato/AgendacontatosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fernando.agendacontato;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendacontatosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
