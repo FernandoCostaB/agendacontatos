f2e91385400c52011315642e05a1f68a5fa8a2c9
FernandoCostaB <fbc@icomp.ufam.edu.br>
2020-10-04 22:13:24 -0400

    Anotações para a criação da tabela de contato na base

--- src/main/java/com/fernando/agendacontato/domain/Contato.java
+++ src/main/java/com/fernando/agendacontato/domain/Contato.java
@@ -2,12 +2,20 @@ package com.fernando.agendacontato.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+@Entity
 public class Contato implements Serializable {
 	
 
 	private static final long serialVersionUID = 1L;
 	
-	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
 	private String sexo;
