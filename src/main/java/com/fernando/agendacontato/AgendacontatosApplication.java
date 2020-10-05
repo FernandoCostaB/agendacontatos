f3c1282be65e39ab9d99d081cbac754343906398
FernandoCostaB <fbc@icomp.ufam.edu.br>
2020-10-04 23:27:01 -0400

    Instanciação de dados

--- src/main/java/com/fernando/agendacontato/AgendacontatosApplication.java
+++ src/main/java/com/fernando/agendacontato/AgendacontatosApplication.java
@@ -1,13 +1,35 @@
 package com.fernando.agendacontato;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class AgendacontatosApplication {
+import com.fernando.agendacontato.domain.Contato;
+import com.fernando.agendacontato.repositories.ContatoRepository;
 
+@SpringBootApplication
+public class AgendacontatosApplication implements CommandLineRunner{
+	
+	@Autowired
+	private ContatoRepository categoriaRepository;
+	
+	
 	public static void main(String[] args) {
 		SpringApplication.run(AgendacontatosApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Contato cont1 = new Contato(null, "fernando", "masculino", "3237-2451", "costa.fb@hotmail.com");
+		Contato cont2 = new Contato(null, "maria", "feminino", "3237-2451", "maria.fb@hotmail.com");
+		
+		categoriaRepository.saveAll(Arrays.asList(cont1,cont2));
+	}
+	
+
+	
 }
